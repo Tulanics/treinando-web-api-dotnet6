dca782ebf0c9a6c4bebbf6cd74487589065898cc
Tulani Silva <tulani.santos@raizen.com>
2023-05-15 17:37:12 -0300

    CRUD Filmes

--- FilmesApi/Data/Dtos/CreateFilmeDto.cs
+++ FilmesApi/Data/Dtos/CreateFilmeDto.cs
@@ -1,16 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace FilmesApi.Models;
+namespace FilmesApi.Data.Dtos;
 
-public class Filme
+public class CreateFilmeDto
 {
-    [Key]
-    [Required]
-    public int Id { get; set; }
-    [Required(ErrorMessage ="O título é obrigatório")]
+    [Required(ErrorMessage = "O título é obrigatório")]
     public string Titulo { get; set; }
     [Required(ErrorMessage = "O gênero é obrigatório")]
-    [MaxLength(50, ErrorMessage ="O Tamanho do gênero não pode exceder 50 caracteres")]
+    [StringLength(50, ErrorMessage = "O Tamanho do gênero não pode exceder 50 caracteres")]
     public string Genero { get; set; }
     [Required(ErrorMessage = "A duração é obrigatório")]
     [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
--- FilmesApi/Program.cs
+++ FilmesApi/Program.cs
@@ -1,8 +1,19 @@
+using FilmesApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
+
+builder.Services.AddDbContext<FilmeContext>(opts =>
+    opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+//adicionar o auto-mapper(leitura automática de DTO)
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+object value = builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
